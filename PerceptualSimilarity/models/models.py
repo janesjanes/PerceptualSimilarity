028e68a04ea32022f83a1556566431e43ca83739
Patsorn <patsorn.sangkloy@gmail.com>
2019-06-29 13:55:06 -0400

    To run as module

--- PerceptualSimilarity/models/models.py
+++ PerceptualSimilarity/models/models.py
@@ -3,9 +3,8 @@ from __future__ import absolute_import
 def create_model(opt):
     model = None
     print(opt.model)
-    from .siam_model import *
+    from dist_model import DistModel
     model = DistModel()
     model.initialize(opt, opt.batchSize, )
     print("model [%s] was created" % (model.name()))
     return model
-
